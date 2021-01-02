107482b2c015854f18dd889c9aa60b8a43bbe904
Lukas90210 <you@example.comlukasgit@interia.pl>
2021-01-02 14:53:15 +0100

    2.04 1

--- aplikacja/src/views/Root/Root.js
+++ aplikacja/src/views/Root/Root.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Button from "../../components/Button/Button";
+import styled from "styled-components";
+
+const Text = styled.text`
+  color: red;
+`;
 
 const Root = () => (
   <div>
     <h1>Hello</h1>
     <Button>button</Button>
+    <Text>
+      <h1>hell</h1>
+    </Text>
   </div>
 );
 
